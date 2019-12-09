33f221fa5e516c23edba20533f7eb0347f2197b9
Luca Castelnuovo <ltcastelnuovo@gmail.com>
2019-12-09 18:44:40 +0100

    added debug and updated netlify config

--- functions/send-email/send-email.js
+++ functions/send-email/send-email.js
@@ -1,125 +1,128 @@
 const sgMail = require('@sendgrid/mail')
-// const Recaptcha = require('google-recaptcha')
-const { validateEmail, validateLength } = require('./validations')
+const Recaptcha = require('google-recaptcha')
+const {
+    validateEmail,
+    validateLength
+} = require('./validations')
 
 
 exports.handler = (event, context, callback) => {
-  if (!process.env.SENDGRID_KEY) {
-    return callback(null, {
-      statusCode: 500,
-      body: 'process.env.SENDGRID_KEY must be defined'
-    })
-  }
-  if (!process.env.RECAPTCHA_KEY) {
-    return callback(null, {
-      statusCode: 500,
-      body: 'process.env.RECAPTCHA_KEY must be defined'
-    })
-  }
-
-  // User data
-  const body = JSON.parse(event.body)
-  try {
-    validateLength('body.g-recaptcha-response', body["g-recaptcha-response"], 256, 1024)
-  } catch (e) {
-    return callback(null, {
-      statusCode: 403,
-      body: e.message
-    })
-  }
-  try {
-    validateLength('body.name', body.name, 3, 50)
-  } catch (e) {
-    return callback(null, {
-      statusCode: 403,
-      body: e.message
-    })
-  }
-  try {
-    validateEmail('body.email', body.email)
-  } catch (e) {
-    return callback(null, {
-      statusCode: 403,
-      body: e.message
-    })
-  }
-  try {
-    validateLength('body.message', body.message, 3, 1000)
-  } catch (e) {
-    return callback(null, {
-      statusCode: 403,
-      body: e.message
-    })
-  }
-
-  // Email settings
-  try {
-    validateLength('body.contact_email', body.contact_email, 16, 256)
-  } catch (e) {
-    return callback(null, {
-      statusCode: 403,
-      body: e.message
-    })
-  }
-  try {
-    validateContactEmail(body.contact_email)
-  } catch (e) {
-    return callback(null, {
-      statusCode: 403,
-      body: e.message
-    })
-  }
-  try {
-    validateLength('body.template_id', body.template_id, 30, 35)
-  } catch (e) {
-    return callback(null, {
-      statusCode: 403,
-      body: e.message
-    })
-  }
-
-  // Compile additional data
-  var additional = [];
-  for (var key in body) {
-    if (body.hasOwnProperty(key) && key !== "name" && key !== "email" && key !== "message" && key !== "g-recaptcha-response") {
-      additional.push(key + ' = ' + body[key]);
+    if (!process.env.SENDGRID_KEY) {
+        return callback(null, {
+            statusCode: 500,
+            body: 'process.env.SENDGRID_KEY must be defined'
+        })
     }
-  };
+    if (!process.env.RECAPTCHA_KEY) {
+        return callback(null, {
+            statusCode: 500,
+            body: 'process.env.RECAPTCHA_KEY must be defined'
+        })
+    }
 
-  // Create message
-  const msg = {
-    to: body.contact_email,
-    from: 'no-reply@lucacastelnuovo.nl',
-    templateId: body.template_id,
-    dynamic_template_data: {
-      name: body.name,
-      email: body.email,
-      details: body.message,
-      additional: additional.join('\n')
-    },
-  };
+    // User data
+    const body = JSON.parse(event.body)
+    try {
+        validateLength('body.g-recaptcha-response', body["g-recaptcha-response"], 256, 1024)
+    } catch (e) {
+        return callback(null, {
+            statusCode: 403,
+            body: e.message
+        })
+    }
+    try {
+        validateLength('body.name', body.name, 3, 50)
+    } catch (e) {
+        return callback(null, {
+            statusCode: 403,
+            body: e.message
+        })
+    }
+    try {
+        validateEmail('body.email', body.email)
+    } catch (e) {
+        return callback(null, {
+            statusCode: 403,
+            body: e.message
+        })
+    }
+    try {
+        validateLength('body.message', body.message, 3, 1000)
+    } catch (e) {
+        return callback(null, {
+            statusCode: 403,
+            body: e.message
+        })
+    }
 
-  // Validate Captcha
-  // const recaptcha = new Recaptcha({secret: process.env.RECAPTCHA_KEY})
-  // recaptcha.verify({response: body["g-recaptcha-response"]}, (error) => {
-  //   if (error) {
-  //     callback(null, {
-  //       statusCode: 500,
-  //       body: error
-  //     })
-  //   }
-  // })
+    // Email settings
+    try {
+        validateLength('body.contact_email', body.contact_email, 16, 256)
+    } catch (e) {
+        return callback(null, {
+            statusCode: 403,
+            body: e.message
+        })
+    }
+    try {
+        validateContactEmail(body.contact_email)
+    } catch (e) {
+        return callback(null, {
+            statusCode: 403,
+            body: e.message
+        })
+    }
+    try {
+        validateLength('body.template_id', body.template_id, 30, 35)
+    } catch (e) {
+        return callback(null, {
+            statusCode: 403,
+            body: e.message
+        })
+    }
 
-  // Send email
-  sgMail.setApiKey(process.env.SENDGRID_KEY);
-  sgMail
-  .send(msg)
-  .then(() => callback(null, {
-    statusCode: 200,
-    body: ''
-  }))
-  .catch(error => callback(null, {
-    statusCode: 500,
-    body: error.message
-  }));
+    // Compile additional data
+    var additional = [];
+    for (var key in body) {
+        if (body.hasOwnProperty(key) && key !== "name" && key !== "email" && key !== "message" && key !== "g-recaptcha-response") {
+            additional.push(key + ' = ' + body[key]);
+        }
+    };
+
+    // Create message
+    const msg = {
+        to: body.contact_email,
+        from: 'no-reply@lucacastelnuovo.nl',
+        templateId: body.template_id,
+        dynamic_template_data: {
+            name: body.name,
+            email: body.email,
+            details: body.message,
+            additional: additional.join('\n')
+        },
+    };
+
+    // Validate Captcha
+    const recaptcha = new Recaptcha({secret: process.env.RECAPTCHA_KEY})
+    recaptcha.verify({response: body["g-recaptcha-response"]}, (error) => {
+      if (error) {
+        callback(null, {
+          statusCode: 500,
+          body: error
+        })
+      }
+    })
+
+    // Send email
+    sgMail.setApiKey(process.env.SENDGRID_KEY);
+    sgMail
+        .send(msg)
+        .then(() => callback(null, {
+            statusCode: 200,
+            body: ''
+        }))
+        .catch(error => callback(null, {
+            statusCode: 500,
+            body: error.message
+        }));
 }
--- functions/send-email/validations.js
+++ functions/send-email/validations.js
@@ -1,53 +1,53 @@
 exports.validateEmail = (ctx, str) => {
-  if (typeof str !== 'string' && !(str instanceof String)) {
-    throw TypeError(`${ctx} must be a string`)
-  }
+    if (typeof str !== 'string' && !(str instanceof String)) {
+        throw TypeError(`${ctx} must be a string`)
+    }
 
-  exports.validateLength(ctx, str, 5, 30)
+    exports.validateLength(ctx, str, 5, 30)
 
-  if (!/^[\w.-]+@[\w.-]+\.\w+$/.test(str)) {
-    throw TypeError(`${ctx} is not an email address`)
-  }
+    if (!/^[\w.-]+@[\w.-]+\.\w+$/.test(str)) {
+        throw TypeError(`${ctx} is not an email address`)
+    }
 }
 
 exports.validateLength = (ctx, str, ...args) => {
-  let min, max
+    let min, max
 
-  if (args.length === 1) {
-    min = 0
-    max = args[0]
-  } else {
-    min = args[0]
-    max = args[1]
-  }
+    if (args.length === 1) {
+        min = 0
+        max = args[0]
+    } else {
+        min = args[0]
+        max = args[1]
+    }
 
-  if (typeof str !== 'string' && !(str instanceof String)) {
-    throw TypeError(`${ctx} must be a string`)
-  }
+    if (typeof str !== 'string' && !(str instanceof String)) {
+        throw TypeError(`${ctx} must be a string`)
+    }
 
-  if (str.length < min) {
-    throw TypeError(`${ctx} must be at least ${min} chars long`)
-  }
+    if (str.length < min) {
+        throw TypeError(`${ctx} must be at least ${min} chars long`)
+    }
 
-  if (str.length > max) {
-    throw TypeError(`${ctx} must contain ${max} chars at most`)
-  }
+    if (str.length > max) {
+        throw TypeError(`${ctx} must contain ${max} chars at most`)
+    }
 }
 
 exports.validateContactEmail = (email) => {
-  if (!process.env.CONFIG_URL) {
-    throw TypeError("process.env.CONFIG_URL must be defined")
-  }
-
-  fetch(process.env.CONFIG_URL)
-  .then((resp) => resp.json())
-  .then(data => {
-    const allowedRecipients = data.send_email.allowed_recipients;
-    if (!allowedRecipients.includes(email)) {
-      throw TypeError(`${email} is not a valid recipient`)
+    if (!process.env.CONFIG_URL) {
+        throw TypeError("process.env.CONFIG_URL must be defined")
     }
-  })
-  .catch(function(error) {
-    console.error(JSON.stringify(error));
-  });
+
+    fetch(process.env.CONFIG_URL)
+        .then((resp) => resp.json())
+        .then(data => {
+            const allowedRecipients = data.send_email.allowed_recipients;
+            if (!allowedRecipients.includes(email)) {
+                throw TypeError(`${email} is not a valid recipient`)
+            }
+        })
+        .catch(function (error) {
+            console.error(JSON.stringify(error));
+        });
 }
